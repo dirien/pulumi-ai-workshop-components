f18eb4df8094d0c85c87b1884dcc9b599212d71f
Neo <neo@pulumi.com>
2025-11-06 14:20:16 +0000

    Add required args parameter to ArgoCDComponent constructor for schema generation

--- index.ts
+++ index.ts
@@ -2,6 +2,13 @@ import * as pulumi from "@pulumi/pulumi";
 import * as k8s from "@pulumi/kubernetes";
 
 /**
+ * Arguments for the ArgoCDComponent.
+ */
+export interface ArgoCDComponentArgs {
+    // Currently no arguments are required, but this interface is needed for component schema generation
+}
+
+/**
  * ArgoCDComponent deploys ArgoCD to a Kubernetes cluster using the official Helm chart.
  * 
  * This component:
@@ -19,11 +26,12 @@ export class ArgoCDComponent extends pulumi.ComponentResource {
      * Creates a new ArgoCD component.
      * 
      * @param name The unique name of the component resource.
+     * @param args Component arguments (currently empty but required for schema generation).
      * @param opts Optional component resource options.
      */
-    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
+    constructor(name: string, args: ArgoCDComponentArgs, opts?: pulumi.ComponentResourceOptions) {
         // Register this component with the Pulumi engine
-        super("ediri:kubernetes:ArgoCDComponent", name, {}, opts);
+        super("ediri:kubernetes:ArgoCDComponent", name, args, opts);
 
         // Create the argocd namespace
         const namespace = new k8s.core.v1.Namespace(
